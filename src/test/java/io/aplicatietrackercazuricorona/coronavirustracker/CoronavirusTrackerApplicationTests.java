17dad2fdec18b3cfc20d44b7ac608f7a833276ba
Sorin Gore <sorin_gore@gitlab.com>
2020-07-13 14:07:51 +0300

    incarcare proiect

---
+++ src/test/java/io/aplicatietrackercazuricorona/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aplicatietrackercazuricorona.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
